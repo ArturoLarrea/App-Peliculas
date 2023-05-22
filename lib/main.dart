e5ff53507631bad0926016a7b4b3eaa62ca7ae12
Arturo Larrea <arthy190@gmail.com>
2023-05-22 17:34:30 -0400

    ImplementandoHttp&Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,28 @@
+import 'package:app_peliculas/provider/movies_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'screens/screens.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const AppState());
+}
+
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => MoviesProvider(),
+          lazy: false,
+        ),
+      ],
+      child: const MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
